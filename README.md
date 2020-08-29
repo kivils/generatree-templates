49690be850b4839ae080f1326f9e4cfcbf2179eb
Yulia Sesyunina <idwebkivils@gmail.com>
2020-08-29 03:49:55 +0300

    Before patch-20200714: Added default RU template. Created readme. Rearranged files in simple template.

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# Templates for Generation Tree
+# Templates for "Generation Tree" application
+
+Templates for [Generation Tree](http://generatree.com/index.html)
+
+## Requirements
+- [NodeJS](https://nodejs.org/en/)
+- [Yarn](https://yarnpkg.com/)
+- [Gulp](https://gulpjs.com/)
+
+## Features
+- development with hot-reload
+- postcss for styling
+- babel for javascript
+
+## Templates
+- **template_1_EN** - default template for EN
+- **template_1_RU** - default template for RU
+- **template_simple_RU** - Simple template
+    - Added mobile phones and tablets support
+    - Removed some sections from HTML (slider, theme switcher, etc.)
+    - Some settings in "Generation Tree" application for site generation ignored (custom homepage, etc.)
+    - Replaced logo with site name
+    - Updated versions of jquery and other libraries
+    - Updated styles
+    - Refactored scripts
+    - Replaced outdated lightbox.js with [fancybox](https://fancyapps.com/fancybox/3/)
+
+(!) Currently only one additional template is created - **Simple for RU**
+
+## Project structure
+- **app/** - source files
+- **dist/** - build files
+- **build/** - gziped templates to use in "Generation Tree" application
+- **gulp-tasks/** - tasks for gulp task runner
+- **gulpfile.babel.js** - main gulp file
+- **gulp-vars.js** - variables for gulp tasks
+    - **GTtemplateName** - name of the active template (on which you're planning to work, and which will be generated)
+    - **injectCss** - list of css files to use in template
+    - **injectJs** - list of js files to use in template
+    - other variables
+
+## Template structure
+
+**template_simple_RU** (new templates must have the same structure to match "Generation Tree" application)
+
+- ***.html** - layouts for pages in main navigation (all files should be kept, even if you don't want to show some of them - otherwise "Generation Tree" application will crash during site generation)
+- **doc/** - empty folder, for images uploaded in "Generation Tree" application
+- **event/event_0.html** - layout for each event
+- **family/family_0.html** - layout for each family
+- **person/person_0.html** - layout for each person
+    - **person/images/** - image folder for persons avatars
+- **tree/tree_t0_0.html** - layout for each tree
+- **tree/trees.html** - layout for full family tree (in main navigation)
+
+
+- **styles/** - source css files
+    **styles.css** - main css file
+- **scripts/** - source javascript files
+    **main.js** - entry point
+- **css/** - generated css files
+- **css/** - generated javascript files
+- **assets/** - duplicates of treejs files (required explicitly for "Generation Tree" application)
+- **fonts/** - fonts for the template
+
+
+## Gulp tasks
+``
+yarn run serve
+``
+
+- Starts local server with hot-reload
+- Active template is set in **gulp-vars.js** via **GTtemplateName** variable
+
+``
+yarn run build
+``
+- Generates dist/ folder with build files
+- Generates zipped template for "Generation Tree" application
+- Bumps version in package.json
+- Bumps version of assets in generated templates (to avoid assets caching issues in browser)
---
+++ app/TEMPLATES/template_1_RU/js/table.init.js
@@ -0,0 +1,263 @@
+$(document).ready(function ($) {
+    /*---------------------------------------------------*/
+    /* Persons DataTable		dom: 'Blfrtip',
+    /*---------------------------------------------------*/    
+    $('#persons').DataTable({
+		colReorder: true,
+		select: true,
+        dom: "<'row'<'col-md-4'B><'col-md-4'l><'col-md-4'f>>" +
+             "<'row'<'col-md-6'><'col-md-6'>>" +
+             "<'row'<'col-md-12't>>" +
+			 "<'row'<'col-md-6'i><'col-md-6'p>>",		
+        buttons: [
+            'colvis'
+        ],
+		columns: [
+			{ "title": "Id", "data": "Id" },
+			{ "title": "Пол", "data": "Пол" },
+			{ "title": "Фамилия", "data": "Фамилия" },
+			{ "title": "Имя", "data": "Имя" },
+			{ "title": "Отчество", "data": "Отчество" },
+			{ "title": "Дата рождения", "data": "Дата рождения" },
+			{ "title": "Место рождения", "data": "Место рождения" },
+			{ "title": "В живых", "data": "В живых" },
+			{ "title": "Дата смерти", "data": "Дата смерти" },
+			{ "title": "Действия", "data": "Действия", "orderable": false,
+                "render": function (data, type, row, meta) {
+                    var result = "<div class=\"dropdown\">\n" +
+                        "  <button class=\"btn btn-default dropdown-toggle\" type=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"true\" title=\"Выбор команды\">\n" +
+                        "    &bull;&bull;&bull;\n" +
+                        "  </button>\n" +
+                        "  <ul class=\"dropdown-menu dropdown-menu-right\">\n" +
+                        "    <li @SHOW_TREE_1@><a href=\"tree/tree_t1_" + row.Id + ".html\">Восходящее дерево</a></li>\n" +
+                        "    <li @SHOW_TREE_2@><a href=\"tree/tree_t2_" + row.Id + ".html\">Нисходящее дерево</a></li>\n" +
+                        "    <li @SHOW_TREE_3@><a href=\"tree/tree_t3_" + row.Id + ".html\">Дерево \"Бабочка\"</a></li>\n" +
+                        "    <li @SHOW_TREE_4@><a href=\"tree/tree_t4_" + row.Id + ".html\">Круговая диаграмма</a></li>\n" +
+                        "  </ul>\n" +
+						"</div>";
+                    return result;
+                }
+			}
+		],
+        language: {
+            decimal: ",",
+            search: "Фильтр:",
+            processing: "Обработка...",
+            lengthMenu: "Отображать по _MENU_ персон",
+            info: "Показано с _START_ по _END_ из _TOTAL_ персон",
+            infoEmpty: "Нет персон",
+            infoFiltered: "(всего _MAX_ персон)",
+            infoPostFix: "",
+            loadingRecords: "<div align='center'><img src='/open-account/images/loading.gif' alt='Загрузка' height='80' /></div>",
+            zeroRecords: "Данные не найдены",
+            emptyTable: "Данные не найдены",
+            paginate: {
+                first: "Первый",
+                previous: "Назад",
+                next: "Далее",
+                last: "Последний"
+            },
+            aria: {
+                sortAscending: ": активировать столбец для сортировки по возрастанию",
+                sortDescending: ": активировать столбец для сортировки в порядке убывания"
+            }
+        }    
+		});
+ 
+    $('#families').DataTable({
+		colReorder: true,
+		select: true,
+        dom: "<'row'<'col-md-4'B><'col-md-4'l><'col-md-4'f>>" +
+             "<'row'<'col-md-6'><'col-md-6'>>" +
+             "<'row'<'col-md-12't>>" +
+			 "<'row'<'col-md-6'i><'col-md-6'p>>",		
+        buttons: [
+            'colvis'
+        ],
+        language: {
+            decimal: ",",
+            search: "Фильтр:",
+            processing: "Обработка...",
+            lengthMenu: "Отображать по _MENU_ семей",
+            info: "Показано с _START_ по _END_ из _TOTAL_ семьи",
+            infoEmpty: "Нет семей",
+            infoFiltered: "(всего _MAX_ семей)",
+            infoPostFix: "",
+            loadingRecords: "<div align='center'><img src='/open-account/images/loading.gif' alt='Загрузка' height='80' /></div>",
+            zeroRecords: "Данные не найдены",
+            emptyTable: "Данные не найдены",
+            paginate: {
+                first: "Первый",
+                previous: "Назад",
+                next: "Далее",
+                last: "Последний"
+            },
+            aria: {
+                sortAscending: ": активировать столбец для сортировки по возрастанию",
+                sortDescending: ": активировать столбец для сортировки в порядке убывания"
+            }
+        }    
+		});
+		
+    $('#events').DataTable({
+		colReorder: true,
+		select: true,
+        dom: "<'row'<'col-md-4'B><'col-md-4'l><'col-md-4'f>>" +
+             "<'row'<'col-md-6'><'col-md-6'>>" +
+             "<'row'<'col-md-12't>>" +
+			 "<'row'<'col-md-6'i><'col-md-6'p>>",		
+        buttons: [
+            'colvis'
+        ],
+        language: {
+            decimal: ",",
+            search: "Фильтр:",
+            processing: "Обработка...",
+            lengthMenu: "Отображать по _MENU_ событий",
+            info: "Показано с _START_ по _END_ из _TOTAL_ событий",
+            infoEmpty: "Нет событий",
+            infoFiltered: "(всего _MAX_ событий)",
+            infoPostFix: "",
+            loadingRecords: "<div align='center'><img src='/open-account/images/loading.gif' alt='Загрузка' height='80' /></div>",
+            zeroRecords: "Данные не найдены",
+            emptyTable: "Данные не найдены",
+            paginate: {
+                first: "Первый",
+                previous: "Назад",
+                next: "Далее",
+                last: "Последний"
+            },
+            aria: {
+                sortAscending: ": активировать столбец для сортировки по возрастанию",
+                sortDescending: ": активировать столбец для сортировки в порядке убывания"
+            }
+        }    
+		});
+		
+    $('#facts').DataTable({
+		paging: false,
+		info: false,
+		colReorder: true,
+		order: [[4, 'asc']],
+        rowGroup: {
+            dataSrc: 4
+        },
+        language: {
+            decimal: ",",
+            search: "Фильтр:",
+            processing: "Обработка...",
+            lengthMenu: "Отображать по _MENU_ фактов",
+            info: "Показано с _START_ по _END_ из _TOTAL_ фактов",
+            infoEmpty: "Нет фактов",
+            infoFiltered: "(всего _MAX_ фактов)",
+            infoPostFix: "",
+            loadingRecords: "<div align='center'><img src='/open-account/images/loading.gif' alt='Загрузка' height='80' /></div>",
+            zeroRecords: "Данные не найдены",
+            emptyTable: "Данные не найдены",
+            paginate: {
+                first: "Первый",
+                previous: "Назад",
+                next: "Далее",
+                last: "Последний"
+            },
+            aria: {
+                sortAscending: ": активировать столбец для сортировки по возрастанию",
+                sortDescending: ": активировать столбец для сортировки в порядке убывания"
+            }
+        }    
+		});
+
+    $('#factsNew').DataTable({
+		paging: false,
+		info: false,
+		colReorder: true,
+		order: [[2, 'asc']],
+        rowGroup: {
+            dataSrc: 2
+        },
+        language: {
+            decimal: ",",
+            search: "Фильтр:",
+            processing: "Обработка...",
+            lengthMenu: "Отображать по _MENU_ фактов",
+            info: "Показано с _START_ по _END_ из _TOTAL_ фактов",
+            infoEmpty: "Нет фактов",
+            infoFiltered: "(всего _MAX_ фактов)",
+            infoPostFix: "",
+            loadingRecords: "<div align='center'><img src='/open-account/images/loading.gif' alt='Загрузка' height='80' /></div>",
+            zeroRecords: "Данные не найдены",
+            emptyTable: "Данные не найдены",
+            paginate: {
+                first: "Первый",
+                previous: "Назад",
+                next: "Далее",
+                last: "Последний"
+            },
+            aria: {
+                sortAscending: ": активировать столбец для сортировки по возрастанию",
+                sortDescending: ": активировать столбец для сортировки в порядке убывания"
+            }
+        }    
+		});
+
+	$('#pers_event').DataTable({
+		colReorder: true,
+		searching: false, 
+		paging: false, 
+		info: false,
+        language: {
+            decimal: ",",
+            search: "Фильтр:",
+            processing: "Обработка...",
+            lengthMenu: "Отображать по _MENU_ событий",
+            info: "Показано с _START_ по _END_ из _TOTAL_ событий",
+            infoEmpty: "Нет событий",
+            infoFiltered: "(всего _MAX_ событий)",
+            infoPostFix: "",
+            loadingRecords: "<div align='center'><img src='/open-account/images/loading.gif' alt='Загрузка' height='80' /></div>",
+            zeroRecords: "Данные не найдены",
+            emptyTable: "Данные не найдены",
+            paginate: {
+                first: "Первый",
+                previous: "Назад",
+                next: "Далее",
+                last: "Последний"
+            },
+            aria: {
+                sortAscending: ": активировать столбец для сортировки по возрастанию",
+                sortDescending: ": активировать столбец для сортировки в порядке убывания"
+            }
+        }    
+		});
+
+	$('#childs').DataTable({
+		colReorder: true,
+		searching: false, 
+		paging: false, 
+		info: false,
+        language: {
+            decimal: ",",
+            search: "Фильтр:",
+            processing: "Обработка...",
+            lengthMenu: "Отображать по _MENU_ детей",
+            info: "Показано с _START_ по _END_ из _TOTAL_ детей",
+            infoEmpty: "Нет детей",
+            infoFiltered: "(всего _MAX_ детей)",
+            infoPostFix: "",
+            loadingRecords: "<div align='center'><img src='/open-account/images/loading.gif' alt='Загрузка' height='80' /></div>",
+            zeroRecords: "Данные не найдены",
+            emptyTable: "Данные не найдены",
+            paginate: {
+                first: "Первый",
+                previous: "Назад",
+                next: "Далее",
+                last: "Последний"
+            },
+            aria: {
+                sortAscending: ": активировать столбец для сортировки по возрастанию",
+                sortDescending: ": активировать столбец для сортировки в порядке убывания"
+            }
+        }    
+		});
+
+});
